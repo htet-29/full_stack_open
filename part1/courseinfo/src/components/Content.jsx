cc3b3294fa0c07c29b75bdbf849f5534935a8c6d
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-07-31 23:43:31 +0630

    Part1.a Exercises

---
+++ part1/courseinfo/src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Part from "../utils/Part.jsx";
+
+const Content = (props) => {
+    return (
+        <div>
+            <Part part={props.part1} exercise={props.exercise1}/>
+            <Part part={props.part2} exercise={props.exercise2}/>
+            <Part part={props.part3} exercise={props.exercise3}/>
+        </div>
+    )
+}
+
+export default Content
