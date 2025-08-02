0a51c12fc9dc2767ad4d222bbfd2153fd3385e5a
htet-29 <htethlaingwin.bsn29@gmail.com>
2025-08-02 19:45:27 +0630

    Exercise 1.3

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -3,9 +3,9 @@ import Part from "../utils/Part.jsx";
 const Content = (props) => {
     return (
         <div>
-            <Part part={props.part1} exercise={props.exercise1}/>
-            <Part part={props.part2} exercise={props.exercise2}/>
-            <Part part={props.part3} exercise={props.exercise3}/>
+            <Part part={props.part1.name} exercise={props.part1.exercises}/>
+            <Part part={props.part2.name} exercise={props.part2.exercises}/>
+            <Part part={props.part3.name} exercise={props.part3.exercises}/>
         </div>
     )
 }
