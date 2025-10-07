1a1e546497ed6c8a0a7405e141c0741eeaa6e38f
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-07 15:46:24 +0630

    Exercise 2.16

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,13 +3,15 @@ import phoneBookService from "./services/phoneBook";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import Notification from "./components/Notification";
 
 const App = () => {
     const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [phNumber, setPhNumber] = useState('');
     const [filterWord, setFilterWord] = useState("");
-    
+    const [notiMessage, setNotiMessage] = useState(null);
+
     const hook = () => {
         phoneBookService
             .getAll()
@@ -19,21 +21,24 @@ const App = () => {
     };
 
     useEffect(hook, []);
-    
+
     const filterPersons = persons.filter(person => person.name.toLowerCase().includes(filterWord));
 
     const addName = (event) => {
         event.preventDefault();
-        
+
         // const isTaken = persons.some((person) => person.name === newPerson.name);
         const person = persons.find(p => p.name === newName);
         if (person) {
-            if (window.confirm(`${person.name} is already added to phonebook, replace the old number with a new one?`)) {
-                const updatePerson = {...person, number: phNumber};
+            if (window.confirm(
+                `${person.name} is already added to phonebook, replace the old number with a new one?`
+            )) {
+                const updatePerson = { ...person, number: phNumber };
                 phoneBookService
                     .update(person.id, updatePerson)
                     .then(returnPerson => {
                         setPersons(persons.map(p => p.id === returnPerson.id ? returnPerson : p));
+                        showNotification(`Updated ${updatePerson.name}'s phone number`);
                         setNewName("");
                         setPhNumber("");
                     })
@@ -49,11 +54,12 @@ const App = () => {
                 .create(newPerson)
                 .then(returnPerson => {
                     setPersons(persons.concat(returnPerson));
+                    showNotification(`Added ${newPerson.name}`);
                     setNewName("");
                     setPhNumber("");
                 })
         }
-        
+
     };
 
     const handleDeleteOf = (id) => {
@@ -72,21 +78,28 @@ const App = () => {
     const handleNumberChange = (event) => {
         setPhNumber(event.target.value);
     };
-    
+
     const handleFilterChange = (event) => {
         setFilterWord(event.target.value);
     }
 
+    function showNotification(message) {
+        setNotiMessage(message);
+        setTimeout(() => {
+            setNotiMessage(null);
+        }, 5000);
+    }
     return (
         <div>
             <h2>Phonebook</h2>
-            <Filter filterWord={filterWord} handleFilterChange={handleFilterChange}/>
+            <Notification message={notiMessage} />
+            <Filter filterWord={filterWord} handleFilterChange={handleFilterChange} />
             <h2>Add a new</h2>
-            <PersonForm addName={addName} handleNameChange={handleNameChange} newName={newName} 
+            <PersonForm addName={addName} handleNameChange={handleNameChange} newName={newName}
                 handleNumberChange={handleNumberChange} phNumber={phNumber}
             />
             <h2>Numbers</h2>
-            <Persons filterPersons={filterPersons} handleDelete={handleDeleteOf}/>
+            <Persons filterPersons={filterPersons} handleDelete={handleDeleteOf} />
         </div>
     );
 };
