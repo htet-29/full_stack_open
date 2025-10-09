2233911860dad273a89ae86fc30fd9ea56f337ec
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-09 21:12:24 +0630

    Exercise 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const persons = [
+let persons = [
     {
         "id": "1",
         "name": "Arto Hellas",
@@ -48,6 +48,13 @@ app.get('/api/persons/:id', (request, response) => {
         response.status(404).end();
     }
 });
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    persons = persons.filter(p => p.id !== id);
+    
+    response.status(204).end();
+})
 
 const PORT = 3001;
 app.listen(PORT, () => {
