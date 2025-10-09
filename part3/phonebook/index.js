38ae93bad6ecce736651cd77692eb3bee0edd958
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-09 22:26:06 +0630

    Exercise 3.7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 app.use(express.json());
+app.use(morgan('tiny'));
 
 let persons = [
     {
@@ -89,4 +91,4 @@ app.post('/api/persons', (request, response) => {
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server is listening to http://localhost:${PORT}`);
-})
+});
