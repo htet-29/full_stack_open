4d867db8711e407542312d5bfc213ba4d33683f5
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-12 13:36:34 +0630

    Exercise 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -78,6 +78,17 @@ app.post('/api/persons', (request, response) => {
     person.save().then(savePerson => response.json(savePerson));
 })
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+    
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id'});
+    }
+    next(error);
+}
+
+app.use(errorHandler);
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is listening to http://localhost:${PORT}`);
