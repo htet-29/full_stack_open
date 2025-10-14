a204aaa109e92b6175831f3b4fb36d950f437021
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-14 21:33:11 +0630

    Exercise 4.5

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -12,7 +12,19 @@ const totalLikes = (blogs) => {
         : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const findMax = (max, current) => {
+        return current.likes > max.likes ? current : max
+    }
+
+    return blogs.length === 0
+        ? 0
+        : blogs.reduce(findMax, blogs[0])
+
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
