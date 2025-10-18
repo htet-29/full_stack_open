2cff8f88e69d90e79fe515e2241359d66b5744ad
Htet Hlaing Win <htethlaingwin.bsn29@gmail.com>
2025-10-18 23:27:08 +0630

    Exercise 4.17

--- part4/blog/controllers/user.js
+++ part4/blog/controllers/user.js
@@ -4,7 +4,7 @@ const User = require('../models/user')
 const { asyncHandler } = require('../utils/wrapper')
 
 userRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1})
     response.json(users)
 })
 
